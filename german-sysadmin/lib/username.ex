5af3f3f8b5c959d262d842d974c36d0d8c955892
Douglas Liebl <douglasliebl@outlook.com>
2023-12-19 21:21:56 -0300

    Add: hardest exercise I've done so far

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,11 +1,21 @@
 defmodule Username do
   def sanitize(username) do
-    case username do
-      "ä" -> "ae"
-      "ö" -> "oe"
-      "ü" -> "ue"
-      "ß" -> "ss"
+    username
+    |> Enum.flat_map(&my_function/1)
+    |> Enum.filter(&allowed_char?/1)
+  end
+
+  defp my_function(char) do
+    case char do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      _ -> [char]
     end
-    # Please implement the sanitize/1 function
+  end
+
+  defp allowed_char?(char) do
+    char in ?a..?z or char == ?_
   end
 end
